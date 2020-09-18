86a21475e6292b848e42e2bebf125876cec21dbd
Julius Gonsior <julius@gaenseri.ch>
2020-09-19 01:16:26 +0200

    Display only active challenges

--- hackzurich/user/views.py
+++ hackzurich/user/views.py
@@ -2,6 +2,7 @@
 """User views."""
 from flask import Blueprint, render_template
 from flask_login import login_required
+from hackzurich.challenge.models import Challenge
 
 blueprint = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
 
@@ -12,4 +13,5 @@ def members():
     """List members."""
     # 1. get list of active challenges
     # display them
-    return render_template("users/members.html")
+    active_challenges = Challenge.query.filter_by(active=True).all()
+    return render_template("users/members.html", active_challenges=active_challenges)
