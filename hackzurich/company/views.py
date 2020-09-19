0e176d8257019603a174a70f0f971bf1b2bf1f91
Julius Gonsior <julius@gaenseri.ch>
2020-09-19 14:46:08 +0200

    Added companies

--- hackzurich/company/views.py
+++ hackzurich/company/views.py
@@ -13,8 +13,7 @@ from flask import (
     url_for,
 )
 from flask_login import login_required, current_user
-from .forms import ChallengeForm
-from .models import Challenge, User_Challenge_Association, Company
+from hackzurich.challenge.models import Challenge, User_Challenge_Association, Company
 from hackzurich.database import db
 import babel
 
@@ -29,81 +28,3 @@ blueprint = Blueprint(
 def display(company_id):
     company = Company.query.filter_by(id=company_id).first()
     return render_template("companies/company.html", company=company)
-
-
-@blueprint.route("/create_new/", methods=["GET", "POST"])
-@login_required
-def create_new():
-    form = ChallengeForm(request.form)
-    if form.validate_on_submit():
-        Challenge.create(
-            challengename=form.challengename.data,
-            description=form.description.data,
-            active=form.active.data,
-        )
-        flash("You've successfully created challenge ", str(form.challengename.data))
-        return redirect(url_for("user.members"))
-    else:
-        flash_errors(form)
-    return render_template("challenges/create_new.html", form=form)
-
-
-@blueprint.route("/mark_failed/<int:challenge_id>")
-@login_required
-def mark_failed(challenge_id):
-    challenge = Challenge.query.filter_by(id=challenge_id).first()
-
-    user_challenge_association = (
-        User_Challenge_Association.query.filter_by(
-            user_id=current_user.id, challenge_id=challenge.id
-        )
-        .order_by(User_Challenge_Association.commited_to_at.desc())
-        .first()
-    )
-    user_challenge_association.done_at = dt.datetime.now()
-    user_challenge_association.succeeded = False
-    db.session.commit()
-
-    flash(
-        "You've aborted challenge "
-        + str(challenge.challengename)
-        + " "
-        + str(user_challenge_association.id)
-    )
-
-    return redirect(url_for("challenge_blueprint.challenge", challenge_id=challenge_id))
-
-
-@blueprint.route("/mark_done/<int:challenge_id>")
-@login_required
-def mark_done(challenge_id):
-    challenge = Challenge.query.filter_by(id=challenge_id).first()
-
-    user_challenge_association = (
-        User_Challenge_Association.query.filter_by(
-            user_id=current_user.id, challenge_id=challenge.id
-        )
-        .order_by(User_Challenge_Association.commited_to_at.desc())
-        .first()
-    )
-    user_challenge_association.done_at = dt.datetime.now()
-    user_challenge_association.succeeded = True
-    db.session.commit()
-
-    flash("You've successfully done challenge " + str(challenge.challengename))
-
-    return redirect(url_for("challenge_blueprint.challenge", challenge_id=challenge_id))
-
-
-@blueprint.route("/commit/<int:challenge_id>")
-@login_required
-def commit(challenge_id):
-    challenge = Challenge.query.filter_by(id=challenge_id).first()
-    user = current_user
-
-    user_challenge_association = User_Challenge_Association.create(
-        user_id=user.id, challenge_id=challenge.id
-    )
-    flash("You've successfully commited to challenge " + str(challenge.challengename))
-
-    return redirect(url_for("challenge_blueprint.challenge", challenge_id=challenge_id))
